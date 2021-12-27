bc412da90ededb3bd111b5e4271777caecdaa189
tanav2202 <tanav.bajaj58@gmail.com>
2021-12-27 08:18:31 +0530

    tables.sql and config.php file added

---
+++ config.php
@@ -0,0 +1,20 @@
+<?php
+ini_set( "display_errors", true );
+date_default_timezone_set( "Australia/Sydney" );  // http://www.php.net/manual/en/timezones.php
+define( "DB_DSN", "mysql:host=localhost;dbname=cms" );
+define( "DB_USERNAME", "username" );
+define( "DB_PASSWORD", "password" );
+define( "CLASS_PATH", "classes" );
+define( "TEMPLATE_PATH", "templates" );
+define( "HOMEPAGE_NUM_ARTICLES", 5 );
+define( "ADMIN_USERNAME", "admin" );
+define( "ADMIN_PASSWORD", "mypass" );
+require( CLASS_PATH . "/Article.php" );
+
+function handleException( $exception ) {
+  echo "Sorry, a problem occurred. Please try later.";
+  error_log( $exception->getMessage() );
+}
+
+set_exception_handler( 'handleException' );
+?>

6d9d0064e4132b56748daaf0968fa6f21d89f385
tanav2202 <tanav.bajaj58@gmail.com>
2021-12-27 08:19:53 +0530

    tables.sql and config.php file added

--- config.php
+++ config.php
@@ -2,13 +2,13 @@
 ini_set( "display_errors", true );
 date_default_timezone_set( "Australia/Sydney" );  // http://www.php.net/manual/en/timezones.php
 define( "DB_DSN", "mysql:host=localhost;dbname=cms" );
-define( "DB_USERNAME", "username" );
-define( "DB_PASSWORD", "password" );
+define( "DB_USERNAME", "root" );
+define( "DB_PASSWORD", "2202" );
 define( "CLASS_PATH", "classes" );
 define( "TEMPLATE_PATH", "templates" );
 define( "HOMEPAGE_NUM_ARTICLES", 5 );
 define( "ADMIN_USERNAME", "admin" );
-define( "ADMIN_PASSWORD", "mypass" );
+define( "ADMIN_PASSWORD", "admin" );
 require( CLASS_PATH . "/Article.php" );
 
 function handleException( $exception ) {
